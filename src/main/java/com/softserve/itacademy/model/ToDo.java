ed014dc02cc2f8301b2fa9e297b3fc76f25a2406
Vadim Batynchuk <batynchukvadim@gmail.com>
2020-10-22 13:46:01 +0300

    ToDo model implementation

--- src/main/java/com/softserve/itacademy/model/ToDo.java
+++ src/main/java/com/softserve/itacademy/model/ToDo.java
@@ -1,16 +1,88 @@
 package com.softserve.itacademy.model;
 
-import org.hibernate.annotations.CreationTimestamp;
+
 import org.hibernate.annotations.GenericGenerator;
+import org.springframework.lang.NonNull;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Set;
 
-
+@Entity
+@Table(name = "todos")
 public class ToDo {
-//TODO
+
+    @Id
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(
+            name = "sequence-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @org.hibernate.annotations.Parameter(name = "sequence_name", value = "todo_sequence"),
+                    @org.hibernate.annotations.Parameter(name = "initial_value", value = "1"),
+                    @org.hibernate.annotations.Parameter(name = "increment_size", value = "1")
+            }
+    )
+    private long id;
+
+    @NotBlank(message = "The titleName cannot be empty")
+    @Column(nullable = false)
+    private String title;
+
+    @Column(name = "created_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @ManyToOne()
+    @JoinColumn(name = "owner_id")
+    @NonNull
+    private User owner;
+
+    public ToDo() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        return "ToDo{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", createdAt=" + createdAt +
+                ", owner=" + owner +
+                '}';
+    }
 }
