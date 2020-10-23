9cb1f9efe703ccb81d9c3fdf26983b6548957cab
Vadim Batynchuk <batynchukvadim@gmail.com>
2020-10-23 11:51:31 +0300

    Tests todo.

---
+++ src/test/java/com/softserve/itacademy/model/ToDoTests.java
@@ -0,0 +1,56 @@
+package com.softserve.itacademy.model;
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class ToDoTests {
+    private static User owner;
+    private static ToDo validToDo;
+
+
+    @BeforeAll
+    static void init() {
+        owner = new User();
+        validToDo = new ToDo();
+        validToDo.setTitle("Valid title");
+        validToDo.setOwner(owner);
+    }
+
+    @Test
+    void toDoWithNotEmptyTitle() {
+
+    }
+    @Test
+    void constraintViolationOnEmptyRoleName() {
+        ToDo emptyTitleToDo = new ToDo();
+        emptyTitleToDo.setTitle("");
+        emptyTitleToDo.setOwner(owner);
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<ToDo>> violations = validator.validate(emptyTitleToDo);
+        assertEquals(1, violations.size());
+    }
+    @Test
+    void constraintViolationOnNullOwner() {
+        ToDo toDoWithoutOwner = new ToDo();
+        toDoWithoutOwner.setTitle("Title");
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        Set<ConstraintViolation<ToDo>> violations = validator.validate(toDoWithoutOwner);
+        assertEquals(1, violations.size());
+    }
+
+}
